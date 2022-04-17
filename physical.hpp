728f08c5ef97262e0ec08b8147d4f4dce7cc8252
Giuseppe <giuseppspathis@gmail.com>
2022-04-17 11:16:15 +0200

    player_animation

--- physical.hpp
+++ physical.hpp
@@ -2,7 +2,12 @@
 #define PHYSICAL_HPP
 
 
-//mettere la struttura della lista degli array
+//struttura della lista degli array dell'animazione del players
+struct animation_player{
+ char player[3][3];
+ animation_player *next;
+ };
+typedef animation_player *p_animation_player;
 
 class Physical {
 	protected:

b2edb171eb7b867f35f06bb844ed2fe1f2f0ec24
Giuseppe <giuseppspathis@gmail.com>
2022-06-28 19:25:42 +0200

    fatto il cronometro

--- timer.cpp
+++ timer.cpp
@@ -1,5 +1,54 @@
 #include "timer.hpp"
 
-Timer::Timer() {
+Timer::Timer(double max_time[]){	
+	for(int i=0; i<n_timers; i++){
+		this->Total_Pauses[i]=0;
+		this->max_time[i]=max_time[i];
+		this->State_pause[i]=false;
+	}	
+};
 
+int Timer::Check_timer(int timer){//spegne se ha superato il tempo limite, ritorna -1 error, 0 false, 1 true
+if(timer<=n_timers && timer >= 0){
+	double curr_time = (double)clock() / CLOCKS_PER_SEC;
+	cout<<"curr_time =" << " " << curr_time<< endl << "timer = " << " " << timers[timer]<< endl;
+	if(curr_time-timers[timer]>max_time[timer]){
+		cout<<"controllo"<<endl;
+		active_timers[timer]=false;
+		return 1;}
+	else
+		return 0;
 }
+return (-1);
+};
+void Timer::Start_timer(int timer){//puoi riattivare un timer già attivo per resettare il suo valore
+	active_timers[timer]=true;
+	timers[timer]=(double)clock() / CLOCKS_PER_SEC;
+};
+void Timer::Start_all_timers(){
+	for(int i=0; i<n_timers; i++){
+		active_timers[i]=true;
+		timers[i]=(double)clock() / CLOCKS_PER_SEC;
+	}
+};
+void Timer::Stop_timer(int timer){
+	active_timers[timer]=false;
+};
+void Timer::Stop_all_timers(){
+	for(int i=0; i<n_timers; i++){
+		active_timers[i]=false;
+	}
+};
+void Timer::Start_Pause(int timer){
+	if(!State_pause[timer]){
+		Start_Pause_value[timer]=(double)clock() / CLOCKS_PER_SEC;
+		State_pause[timer]=true;
+	}	
+};
+void Timer::Finish_Pause(int timer){
+	if(State_pause[timer]){
+		double Finish_Pause=(double)clock() / CLOCKS_PER_SEC;
+		Total_Pauses[timer]=Total_Pauses[timer] + (Finish_Pause - Start_Pause_value[timer]);
+		State_pause[timer]=false;
+	}
+};
--- timer.hpp
+++ timer.hpp
@@ -4,17 +4,27 @@
 #include <iostream>
 #include <ctime>
 
+const int n_timers=5;
+
 class Timer {
-	private:
+	protected:
+		double timers[n_timers];
+		bool active_timers[n_timers];
+		double max_time[n_timers];
+		double Start_Pause_value[n_timers];
+		double Total_Pauses[n_timers];
+		bool State_pause[n_timers];
 	
 	public:
-		Timer();
-	
-		
-
+		Timer(double max_time[]);
+		int Check_timer(int timer);//timer sarebbe la posizione dell'array che indica lo specifico cronometro
+		void Start_timer(int timer);
+		void Start_all_timers();
+		void Update_timers();
+		void Stop_timer(int timer);
+		void Stop_all_timers();
+		void Start_Pause(int timer);
+		void Finish_Pause(int timer);
 };
-
-
-
 
 #endif

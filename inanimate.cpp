b2004832bb73ca9dc2022194ac5012f9edfa10cb
papum20_lenovo <danieledugo1@gmail.com>
2022-07-17 12:17:37 +0200

    inzio risoluzione x virtual

--- inanimate.cpp
+++ inanimate.cpp
@@ -12,9 +12,9 @@ void Inanimate::init_colors() {
 void Inanimate::drawCell(Cell scr[CAMERA_HEIGHT][CAMERA_WIDTH], Coordinate win_start, Coordinate pos, attr_t color) {
 	pos.setFullMatrix(win_start.x, win_start.y, -1, -1);
 	if(pos.inOwnBounds())
-		scr[pos.relative_x()][pos.relative_y()].edit(-1, -1, color, -1);
+		scr[(int)pos.relative_x()][(int)pos.relative_y()].edit(-1, -1, color, -1);
 }
 void Inanimate::drawAtPosition(Cell scr[CAMERA_HEIGHT][CAMERA_WIDTH], Coordinate win_start, Coordinate pos) {
-	if(scr[pos.y][pos.x].getCh() == CHAR_EMPTY) drawCell(scr, win_start, pos, main_color);
+	if(scr[pos.inty()][pos.intx()].getCh() == CHAR_EMPTY) drawCell(scr, win_start, pos, main_color);
 	else drawCell(scr, win_start, pos, second_color);
 }

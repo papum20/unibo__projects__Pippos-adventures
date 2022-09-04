4306c620ed29d57196ad273165f3c061bfeede98
Osama <osama.elatfi@gmail.com>
2022-09-04 16:30:05 +0200

    aa

--- item_difensivi.hpp
+++ item_difensivi.hpp
@@ -17,7 +17,7 @@ class item_difensivo: public item{
 
         void apply_rune();
         void apply_rarity();
-        virtual char get_type(){};
+        virtual char get_type();
 };
 
 typedef item_difensivo* pItem_def;

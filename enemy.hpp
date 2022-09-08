94877485df5eefc61f870ef69eccc60396a70837
Osama <osama.elatfi@gmail.com>
2022-09-08 19:02:23 +0200

    aaa

--- enemy.hpp
+++ enemy.hpp
@@ -9,11 +9,22 @@
 //ha una sottoclasse per ogni tipo di nemico
 
 const int enemy_stamina=50;
+const int enemy_refreshing_rate=3;
+const int chase_distance=20;
+const int enemy_vision=30;
+const int initial_step=0;
+const int max_steps=enemy_refreshing_rate*2;
 
 class Enemy : public Character {
 	protected:
 		int points_given;
 		pCharacter player;
+		Coordinate memorized_path[max_steps];
+		bool player_in_vision;
+		bool player_found;
+		int frames_passed;
+		int memorized_steps;
+		int current_step;
 	public:
 		Enemy();
 		Enemy(pCharacter p);
@@ -21,6 +32,7 @@ class Enemy : public Character {
 		void destroy(pMap map);
 		void copyEnemy(Enemy B);			//copia i parametri di B
 
+		void make_step(pMap map);
 		void update(pMap map);
 		void check_enemy_melee(pMap map);
 		void meleeIA(pMap map);

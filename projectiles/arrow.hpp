6663c381aed939e45748856d36c2de651346027d
papum20_lenovo <danieledugo1@gmail.com>
2022-09-09 20:57:12 +0200

    door

--- projectiles/arrow.hpp
+++ projectiles/arrow.hpp
@@ -9,35 +9,40 @@ const int arrow_down_index=1;
 const int arrow_left_index=2;
 const int arrow_right_index=3;
 
-const int arrow_horizontal_height=1;
+const int arrow_horizontal_height=2;
 const int arrow_horizontal_width=3;
-const int arrow_vertical_height=3;
+const int arrow_horizontal_depth=1;
+const int arrow_vertical_height=4;
 const int arrow_vertical_width=1;
+const int arrow_vertical_depth=3;
 
 const char arrow_right[ANIMATION_HEIGHT][ANIMATION_WIDTH]=
 { 
-   {'-', '-', '>'} 
+   {'-', '-', '>'} ,
+   {' ', ' ', ' '}
    
 };
 
 const char arrow_left[ANIMATION_HEIGHT][ANIMATION_WIDTH]=
 { 
-   {'<', '-', '-'} 
-  
+   {'<', '-', '-'},
+   {' ', ' ', ' '}
 };
 
 const char arrow_up[ANIMATION_HEIGHT][ANIMATION_WIDTH]=
 { 
    {'^'},
    {'|'},
-   {'|'}
+   {'|'},
+   {' '}
 };
 
 const char arrow_down[ANIMATION_HEIGHT][ANIMATION_WIDTH]=
 {   
    {'|'},
    {'|'},
-   {'v'}
+   {'v'},
+   {' '}
 };
 
 class Arrow:public Projectile{
